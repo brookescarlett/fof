4c0c22304fe377c3d1db24998ff1f0a3c0734891
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-10-10 13:34:45 -0400

    template for description component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Slider from './components/slider'
+import Content from './components/content'
 
 import './App.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return (
      <div className="fixed top-0 left-0 right-0 bottom-0">
+        <Content />
         <Slider />
      </div>
     );
---
+++ src/components/content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+export default class Content extends Component {
+  render() {
+    return (
+        <div className="absolute content-container">
+            <p className="h1 title">The Maya</p>
+            <p className="desciption"> The Maya is a classic serape, re-imagined and recreated in luxurious fabrics to bring new life and contemporary relevance to a Latin American tradition.</p>
+            <div className="button p2 h6 center">shop the maya</div>
+        </div>
+    );
+  }
+}
